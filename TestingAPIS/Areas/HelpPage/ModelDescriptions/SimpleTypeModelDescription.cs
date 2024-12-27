7dc4ed53547ed5810c020b341a634bdad59bd4f7
Hardik Gosai <scspl.hardikg@gmail.com>
2024-12-27 10:35:29 +0530

    Add Project
    Add Project in Git

---
+++ TestingAPIS/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace TestingAPIS.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ TestingAPIS/Controllers/StudentController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TestingAPIS.Models;
+
+namespace TestingAPIS.Controllers
+{
+    [RoutePrefix("Api/Student")]
+    public class StudentController : ApiController
+    {
+        TestDBEntities DB = new TestDBEntities();
+
+        [Route("AddOrUpdateStudent")]
+        [HttpPost]
+        public object AddotrUpdatestudent(Student st)
+        {
+            try
+            {
+                if (st.Id == 0)
+                {
+                    studentmaster sm = new studentmaster();
+                    sm.name = st.Name;
+                    sm.rollno = st.Rollno;
+                    sm.address = st.Address;
+                    sm.@class = st.Class;
+                    DB.studentmasters.Add(sm);
+                    DB.SaveChanges();
+                    return new Response
+                    {
+                        Status = "Success",
+                        Message = "Data Successfully"
+                    };
+                }
+                else
+                {
+                    var obj = DB.studentmasters.Where(x => x.Id == st.Id).ToList().FirstOrDefault();
+                    if (obj.Id > 0)
+                    {
+                        obj.name = st.Name;
+                        obj.rollno = st.Rollno;
+                        obj.address = st.Address;
+                        obj.@class = st.Class;
+                        DB.SaveChanges();
+                        return new Response
+                        {
+                            Status = "Updated",
+                            Message = "Updated Successfully"
+                        };
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.Message);
+            }
+            return new Response
+            {
+                Status = "Error",
+                Message = "Data not insert"
+            };
+        }
+
+        [Route("GetAllStudentDetails")]
+        [HttpGet]
+        public object Studentdetails()
+        {
+            var a = DB.studentmasters.ToList();
+            return a;
+        }
+
+        [Route("GetByIdStudentDetail")]
+        [HttpGet]
+        public object StudentdetailById(int id)
+        {
+            var obj = DB.studentmasters.Where(x => x.Id == id).ToList().FirstOrDefault();
+            return obj;
+        }
+
+        [Route("DeleteStudentById")]
+        [HttpDelete]
+        public object Deletestudent(int id)
+        {
+            var obj = DB.studentmasters.Where(x => x.Id == id).ToList().FirstOrDefault();
+            DB.studentmasters.Remove(obj);
+            DB.SaveChanges();
+            return new Response
+            {
+                Status = "Delete",
+                Message = "Delete Successfuly"
+            };
+        }
+    }
+}
